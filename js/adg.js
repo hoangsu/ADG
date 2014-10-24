53ef737f167e4052e0bbc73fe71d0084b0c76972
hoangdt <hoangdt@smartosc.com>
2014-10-24 18:14:09 +0700

    day 2 - index.html

---
+++ js/adg.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+	$('.menu > ul > li').hover(function() {
+		$(this).find('.sub-menu .sm-item').each(function(key,val) {
+			$(this).find('.sm-content').css('top', -30*key);
+		});
+		$(this).find('.sub-menu').show('fast', function() {
+			$('.sm-item.first > a').css({
+				background: '#00529c',
+				color: '#fff'
+			});
+			$('.sm-item.first .sm-content').show();
+			$('.sub-menu .sm-item > a').hover(function() {
+				$(this).next('.sm-content').show();
+				if (!$(this).parent('.sm-item').hasClass('first')) {
+					$('.sm-item.first .sm-content').hide()
+					$('.sm-item.first > a').css({
+						background: 'none',
+						color: 'inherit'
+					});
+				}
+				else {
+					$(this).css({
+						background: '#00529c',
+						color: '#fff'
+					});
+				};
+			}, function() {
+				$(this).next('.sm-content').hide();
+				$('.sm-item.first > a').css({
+					background: '#00529c',
+					color: '#fff'
+				});
+				$('.sm-item.first .sm-content').show()
+			});	
+		});
+	}, function() {
+		$(this).find('.sub-menu').hide();
+	});
+	
+});
